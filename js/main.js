6ce6c0fc8527c25b445f78e3ef5472d8a8db80ea
Han Heul Jo <hanheuljo@gmail.com>
2017-12-23 14:32:37 +0900

    Basic world creation
    Created world and drawing notes (boxes) for starters.
    TODO:: fix keydown event triggering twice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var worldWidth = document.documentElement.clientWidth
-var worldHeight = document.documentElement.clientHeight
+var worldWidth = window.innerWidth;
+var worldHeight = window.innerHeight;
+var objLimit = 50;
 
 // module aliases
 var Engine = Matter.Engine,
@@ -20,11 +21,16 @@ var render = Render.create({
 	}
 });
 
+engine.world.bounds.max.x = worldWidth;
+engine.world.bounds.max.y = worldHeight;
+
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
 var boxC = Bodies.rectangle(450, 50, 80, 80);
 var ground = Bodies.rectangle(worldWidth / 2, worldHeight - 20, worldWidth - 250, 60, { isStatic: true });
+
+var notes = [];
 
 // add all of the bodies to the world
 World.add(engine.world, [boxA, boxB, ground]);
@@ -35,6 +41,36 @@ Engine.run(engine);
 // run the renderer
 Render.run(render);
 
-function addNote() {
-	World.add(engine.world, [boxC]);
+function createNote(x, y, key) {
+	return Matter.Body.create({
+		position: {
+			x: x,
+			y: y
+		},
+		mass: 0.0017,
+		friction: 0,
+		restitution: 1
+	});
 }
+
+function addNote(e) {
+	var note = createNote(worldWidth / 2, worldHeight - 300, e);
+	note.angle = Math.random() * 0.5 - 0.25;
+	note.force.y -= 0.00015;
+
+	displayNote(note);
+}
+
+function displayNote(note) {
+	notes.push(note);
+	World.add(engine.world, notes[notes.length - 1]);
+
+	if (notes.length > objLimit) {
+		World.remove(engine.world, notes[0]);
+		notes = notes.slice(1);
+	}
+}
+
+$(document).keydown(function(e) {
+	addNote(e);
+});
