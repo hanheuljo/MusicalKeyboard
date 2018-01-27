e4355f13b5bbc370e2dbd4b94d3e6c95d82e377f
Han Heul Jo <hanheuljo@gmail.com>
2018-01-27 13:54:18 +0900

    Note drawing scheme change
    Notes are drawn only when the key is being pressed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var worldWidth = window.innerWidth;
 var worldHeight = window.innerHeight;
-var objLimit = 50;
+var objLimit = 5000;
+var bps = 30;
+var beatsCounted = 0;
+
+var notes = [];
 
 // module aliases
 var Engine = Matter.Engine,
@@ -26,7 +30,7 @@ engine.world.bounds.max.y = worldHeight;
 
 var ground = Bodies.rectangle(worldWidth / 2, worldHeight - 20, worldWidth - 250, 60, { isStatic: true });
 
-var notes = [];
+var bodies = [];
 
 // add all of the bodies to the world
 World.add(engine.world, [ground]);
@@ -67,33 +71,86 @@ function addNote(e) {
 		return;
 	}
 
+	notes.push(key);
+
+/**
 	var x_pos = 200 + pos * (worldWidth - 400) / 9;
 	var y_pos = worldHeight - 50;
 
 	var note = createNote(x_pos, y_pos, key);
 	displayNote(note);
+**/
 }
 
 function removeNote(e) {
-	console.log(e.originalEvent.key);
+	var str = "qwertyuiop";
+	var key = e.originalEvent.key;
+	var pos = str.indexOf(key);
+
+	if (pos == -1) {
+		return;
+	}
+
+	var index = notes.indexOf(key);
+	if (index > - 1) {
+		notes.splice(index, 1);
+	}
 }
 
 // pushes note into display queue
 function displayNote(note) {
-	notes.push(note);
-	World.add(engine.world, notes[notes.length - 1]);
+	bodies.push(note);
+	World.add(engine.world, bodies[bodies.length - 1]);
 
-	if (notes.length > objLimit) {
-		World.remove(engine.world, notes[0]);
-		notes = notes.slice(1);
+	if (bodies.length > objLimit) {
+		World.remove(engine.world, bodies[0]);
+		bodies = bodies.slice(1);
+	}
+}
+
+// function triggered each beat count, called by countBeat
+// draws objects
+function drawNote(note) {
+	var str = "qwertyuiop";
+	var key = note;
+	var pos = str.indexOf(key);
+
+	var x_pos = 200 + pos * (worldWidth - 400) / 9;
+	var y_pos = worldHeight - 50;
+
+	var note = createNote(x_pos, y_pos, key);
+
+	bodies.push(note);
+	World.add(engine.world, bodies[bodies.length - 1]);
+
+	if (bodies.length > objLimit) {
+		World.remove(engine.world, bodies[0]);
+		bodies = bodies.slice(1);
+	}
+}
+
+// function triggered each beat count
+function countBeat(notes) {
+	var length = notes.length;
+
+	for (i = 0; i < length; i++) {
+		var note = notes[i];
+
+		// draws each note
+		drawNote(note);
+		// plays each note
+
 	}
 }
 
 // keydown handler
 $(document).keydown(function(e) {
 	if (!(e.handled)) {
-		addNote(e);
-		e.handled = true;
+		isRepeat = e.originalEvent.repeat;
+		if (!isRepeat) {
+			addNote(e);
+			e.handled = true;
+		}
 	}
 });
 
@@ -104,3 +161,11 @@ $(document).keyup(function(e) {
 		e.handled = true;
 	}
 });
+
+// calls countBeat repeatedly
+(function renderNotes(){
+	beatsCounted++;
+	countBeat(notes);
+
+	setTimeout(arguments.callee, 1000 / bps);
+})();
